234e8ef69f91b0d66847cdfe70d12e1f57e8e329
Alan Ludke <alanludke@gmail.com>
2021-07-31 16:41:57 -0300

    methods splitElements and getInternalRegex added

--- src/main/java/service/RegularExpressionService.java
+++ src/main/java/service/RegularExpressionService.java
@@ -1,13 +1,15 @@
 package service;
 
 import dto.FiniteAutomataDTO;
+import dto.Tree;
 import dto.Node;
 import dto.TransitionDTO;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.ArrayList;
+
 
 public class RegularExpressionService {
 
@@ -29,4 +31,51 @@ public class RegularExpressionService {
 
         return finiteAutomata;
     }
+    
+    
+    public List splitElements(Tree tree, String regex) {
+        List<String> elements_list = new ArrayList<String>();
+        Integer aux = 0;
+        
+        for (int i = 0; i < regex.length(); i++) {
+            if((i + aux) == regex.length()){
+                break;
+            }
+
+            String c = String.valueOf(regex.charAt(i + aux));
+
+            if(tree.getAlphabet().contains(c) || "#|&".contains(c)) {
+                elements_list.add(c);
+            }
+            if("*".equals(c)) {
+                if(elements_list.get(i - 1).length()> 1 && elements_list.size() > 1) {
+                    elements_list.set(i-1, "(" + elements_list.get(i - 1) + ")*");
+                } else {
+                    elements_list.add("*");
+                }
+            } else if("(".equals(c)){
+                elements_list.add(getInternalRegex(regex.substring(i+aux+1, regex.length()-1)));
+                aux += elements_list.get(elements_list.size()-1).length() + 1;
+            }
+        }
+        return elements_list;
+    }
+    
+    public static String getInternalRegex(String regex) {
+        String internal_regex = "";
+        List<String> stack = new ArrayList<String>();
+        
+        for (String c : stack) {
+            if("(".equals(c)){
+                stack.add(c);
+            } else if(stack.isEmpty()){
+                return internal_regex;
+            } else {
+                stack.remove(stack.size()-1);
+            }
+            internal_regex += c;
+        }
+    return "";
+    }
+    
 }
